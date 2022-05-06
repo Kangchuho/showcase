80accf1277777cadc416538ff016e7d12fe73f63
unknown <reddokk@gmail.com>
2022-05-06 10:30:07 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper, Button } from './components';
+import assets from './assets';
+import styles from './styles/Global';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+        title="You own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy store, collection NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design"
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' device. You can easly get your app inot pelple's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contaions two screen. The first screen lists all NFTs while the second one shows the detailes of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-cneter items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made whth love by <span className="blod">JavaScript Mastery</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
